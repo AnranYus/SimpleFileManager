adf52bff96d6a67b705f959ae2472dcabcf95410
Anran <anranyus@hotmail.com>
2023-04-26 22:56:56 +0800

    Change default path define method && Fix wrong path separator

--- src/main/java/com/anranyus/filemanager/simplefilemanager/SimpleFileManagerApplication.java
+++ src/main/java/com/anranyus/filemanager/simplefilemanager/SimpleFileManagerApplication.java
@@ -1,13 +1,7 @@
 package com.anranyus.filemanager.simplefilemanager;
 
-import com.anranyus.filemanager.simplefilemanager.service.FileService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.system.ApplicationHome;
-import org.springframework.core.env.Environment;
-
-import java.io.File;
 
 @SpringBootApplication
 public class SimpleFileManagerApplication {
